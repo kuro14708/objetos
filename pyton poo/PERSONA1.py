ada666b27317606e3feee8d793b7a4d9ad6ac8b8
kuro14708 <52222000+kuro14708@users.noreply.github.com>
2019-09-26 21:24:42 -0500

    Add files via upload

--- pyton poo/PERSONA1.py
+++ pyton poo/PERSONA1.py
@@ -4,7 +4,29 @@ class   persona:
      
 
     def __init__(self, nombre , edad , cc):
-         self.nombre = "mostacho"
-         self.edad = 32
 
+        self.nombre = nombre
+        
+        self.cc = cc
+        
+        self.edad = edad
+
+    def estudio(self):
+       print("Formacion en programacion")
     
+    def fechaNacimiento(self):
+       print("nacio en Junio 1987")
+
+    def getnombre(self):
+        return self.nombre
+    
+
+    def setnombre(self, nombre):
+        if isinstance(nombre, int):
+            self.nombre = nombre
+        else:
+            return "la persona no esta registrada"
+
+nuevoAdsi =persona("kuro","185164",19)
+print(nuevoAdsi.setnombre("500"))
+print(nuevoAdsi.getnombre())
