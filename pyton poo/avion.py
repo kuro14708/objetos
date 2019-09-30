6e7523c1b902d15be4f2dea5ea081ce629d28f0d
kuro14708 <52222000+kuro14708@users.noreply.github.com>
2019-09-30 18:11:51 -0500

    Add files via upload

--- pyton poo/avion.py
+++ pyton poo/avion.py
@@ -3,4 +3,14 @@ class avion:
   #repetir esto 6 veces cambiabdo el nombre
    def avion (self):
        print()
+   def vuelo (self):
+       print()
+   def pasajeros (self):
+       print()
+   def destino (self):
+       print()
+   def origen (self):
+       print()
+   def clima (self):
+       print()
 
